8888ad6d33f838bee8fddc15df2c61d505e3b738
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-05-07 10:45:16 +0300

    feat(ir): added ir

--- include/pin_config.h
+++ include/pin_config.h
@@ -20,6 +20,15 @@
 #define LEFT_PWM_CHANNEL   TIMER1_CHANNEL_A   // D11
 #define RIGHT_PWM_CHANNEL  TIMER1_CHANNEL_B   // D12
 
+#define IR_LEFT_PORT   PORT_C
+#define IR_LEFT_PIN    0   // D37
+
+#define IR_RIGHT_PORT  PORT_C
+#define IR_RIGHT_PIN   1   // D36
+
+#define IR_FRONT_PORT  PORT_C
+#define IR_FRONT_PIN   2   // D35
+
 #define TRIG_PORT  PORT_B
 #define TRIG_PIN   1
 
