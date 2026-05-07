00abf26cbdc72cd8dc795bbb981696794b6d52ce
Ahmedianoo <ahmed.mohamed04@hotmail.com>
2026-05-07 12:46:30 +0300

    feat(fsm): added state update function

---
+++ src/fsm/fsm.h
@@ -0,0 +1,29 @@
+#ifndef FSM_H
+#define FSM_H
+
+#include "std_types.h"
+
+
+typedef enum
+{
+    STATE_DEFAULT = 0,
+
+    STATE_POSSIBLE_RIGHT_CORNER,
+    STATE_RIGHT_CORNER,
+    STATE_POST_RIGHT_CORNER,
+
+    STATE_POSSIBLE_LEFT_CORNER,
+    STATE_LEFT_CORNER,
+    STATE_POST_LEFT_CORNER,
+
+    STATE_RIGHT_ALIGN,
+    STATE_LEFT_ALIGN,
+
+    STATE_FINISH
+
+} FSM_State_t;
+
+FSM_State_t FSM_UpdateState(uint8 l, uint8 f, uint8 r, FSM_State_t currentState);
+
+
+#endif
