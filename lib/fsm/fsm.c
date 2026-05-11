6a444019057eb8f14b86c72dd218c691405c896e
Omar Mahmoud Demerdash <omardemerdash.2027@gmail.com>
2026-05-11 16:01:27 +0300

    Fix main

--- lib/fsm/fsm.c
+++ lib/fsm/fsm.c
@@ -8,115 +8,52 @@ FSM_State_t FSM_UpdateState(uint8 l, uint8 f, uint8 r, FSM_State_t currentState,
         /* ================= DEFAULT ================= */
         case STATE_DEFAULT:
 
-            if (l == 1 && f == 0 && r == 0)
-                return STATE_POSSIBLE_RIGHT_CORNER;
-
-            if (l == 0 && f == 0 && r == 1)
-                return STATE_POSSIBLE_LEFT_CORNER;
-
-            if (l == 0 && f == 0 && r == 0) // you may remove this center f == 0
-                return STATE_FINISH;                
-
+            if (l == 1 && f == 0 && r == 0) return STATE_POSSIBLE_RIGHT_CORNER;
+            if (l == 0 && f == 0 && r == 1) return STATE_POSSIBLE_LEFT_CORNER;
+            if (l == 0 && f == 0 && r == 0) return STATE_FINISH;
             return STATE_DEFAULT;
 
 
         /* ========== POSSIBLE RIGHT CORNER ========== */
         case STATE_POSSIBLE_RIGHT_CORNER:
 
-            if (l == 1 && f == 0 && r == 1)
-                return STATE_DEFAULT;
-
-            if (l == 1 && f == 1 && r == 0)
-                return STATE_RIGHT_CORNER;
-
-            if ((l == 0 && f == 0 && r == 0) && (distances[0] > 45 && distances[1] > 45 && distances[2] > 45))
-                return STATE_FINISH;
-
+            if (l == 1 && f == 1) return STATE_RIGHT_CORNER;    // dead-end confirmed
+            if (l == 1 && r == 1) return STATE_DEFAULT;          // T-junction passed
+            if (l == 0 && f == 0 && r == 0) return STATE_FINISH;
             return STATE_POSSIBLE_RIGHT_CORNER;
-
-
-        /* ========== POSSIBLE LEFT CORNER ========== */
-        case STATE_POSSIBLE_LEFT_CORNER:
-
-            if (l == 1 && f == 0 && r == 1)
-                return STATE_DEFAULT;
-
-            if (l == 0 && f == 1 && r == 1)
-                return STATE_LEFT_CORNER;
-
-            if ((l == 0 && f == 0 && r == 0) && (distances[0] > 45 && distances[1] > 45 && distances[2] > 45))
-                return STATE_FINISH;
-
-            return STATE_POSSIBLE_LEFT_CORNER;
 
 
         /* ========== RIGHT CORNER ========== */
         case STATE_RIGHT_CORNER:
 
-            if (l == 1 && f == 0 && r == 0)
-                return STATE_POST_RIGHT_CORNER;
-
+            // Turn right while front is blocked; move forward after.
+            // Exit once fully in the new corridor (both walls, clear front).
+            if (l == 1 && f == 0 && r == 1) return STATE_DEFAULT;
             return STATE_RIGHT_CORNER;
+
+
+        /* ========== POSSIBLE LEFT CORNER ========== */
+        case STATE_POSSIBLE_LEFT_CORNER:
+
+            if (f == 1 && r == 1) return STATE_LEFT_CORNER;     // dead-end confirmed
+            if (l == 1 && r == 1) return STATE_DEFAULT;          // T-junction passed
+            if (l == 0 && f == 0 && r == 0) return STATE_FINISH;
+            return STATE_POSSIBLE_LEFT_CORNER;
 
 
         /* ========== LEFT CORNER ========== */
         case STATE_LEFT_CORNER:
 
-            if (l == 0 && f == 0 && r == 1)
-                return STATE_POST_LEFT_CORNER;
-
+            // Turn left while front is blocked; move forward after.
+            // Exit once fully in the new corridor (both walls, clear front).
+            if (l == 1 && f == 0 && r == 1) return STATE_DEFAULT;
             return STATE_LEFT_CORNER;
-
-
-        /* ========== POST RIGHT CORNER ========== */
-        case STATE_POST_RIGHT_CORNER:
-
-            if (l == 1  && r == 1) //&& f == 0
-                return STATE_DEFAULT;
-
-            // if (l == 1 && f == 0 && r == 0)
-            //     return STATE_POSSIBLE_RIGHT_CORNER;
-
-            if (l == 0 && f == 0 && r == 1)
-                return STATE_POSSIBLE_LEFT_CORNER;                
-
-            // if (l == 1 && f == 1 && r == 0) // could be removed if confirmed not in the competition
-            //     return STATE_RIGHT_CORNER;  
-                
-            // if (l == 1 && f == 1 && r == 0)
-            //     return STATE_RIGHT_CORNER;                
-
-            return STATE_POST_RIGHT_CORNER;
-
-
-        /* ========== POST LEFT CORNER ========== */
-        case STATE_POST_LEFT_CORNER:
-
-            if (l == 1  && r == 1) //&& f == 0
-                return STATE_DEFAULT;
-
-            if (l == 1 && f == 0 && r == 0)
-                return STATE_POSSIBLE_RIGHT_CORNER;                
-
-            // if (l == 0 && f == 1 && r == 1)
-            //     return STATE_LEFT_CORNER;                
-
-            return STATE_POST_LEFT_CORNER;
-
-
-        /* ========== ALIGN STATES (to be modified to call the right aligned function) ========== */
-        case STATE_RIGHT_ALIGN:
-            return STATE_DEFAULT;
-        case STATE_LEFT_ALIGN:
-            return STATE_DEFAULT;
 
 
         /* ========== FINISH ========== */
         case STATE_FINISH:
 
-            if (l == 1 && f == 0 && r == 1)
-                return STATE_DEFAULT;
-
+            if (l == 1 && f == 0 && r == 1) return STATE_DEFAULT;
             return STATE_FINISH;
 
 
--- src/main.c
+++ src/main.c
@@ -119,49 +119,21 @@ int main(void)
 
         while (currentState == STATE_RIGHT_CORNER)
         {
-
             if (rightTurnSaved == 0)
             {
                 turnSequence[turnCount] = 'R';
                 turnCount++;
-
                 rightTurnSaved = 1;
-            }  
+            }
 
-            Car_TurnRight();          
-
-            ultrasonicReadAllCm(distances, MAX_ECHO_US);
-            ultrasonicCheckThresholds(distances, thresholds);
-            leftSensor  = thresholds[0];
-            frontSensor = thresholds[1];
-            rightSensor = thresholds[2];
-
-            currentState = FSM_UpdateState(
-                                leftSensor,
-                                frontSensor,
-                                rightSensor,
-                                currentState, 
-                                NULL
-                           );                       
-        }
-
-
-        /* ========= POST RIGHT CORNER ========= */
-
-        while (currentState == STATE_POST_RIGHT_CORNER)
-        {
-            if (distances[0] < LEFT_MIN_DISTANCE)
-            {
+            if (frontSensor == 1)
+                Car_TurnRight();
+            else if (distances[0] < LEFT_MIN_DISTANCE)
                 Car_LeftAlign();
-            }
-            else if (distances[1] < POST_CORNER_MIN_DISTANCE)
-            {
+            else if (distances[2] < RIGHT_MIN_DISTANCE)
                 Car_RightAlign();
-            }
             else
-            {
                 Car_MoveForward();
-            }
 
             ultrasonicReadAllCm(distances, MAX_ECHO_US);
             ultrasonicCheckThresholds(distances, thresholds);
@@ -173,9 +145,9 @@ int main(void)
                                 leftSensor,
                                 frontSensor,
                                 rightSensor,
-                                currentState, 
+                                currentState,
                                 NULL
-                           );                        
+                           );
         }
 
 
@@ -219,49 +191,21 @@ int main(void)
 
         while (currentState == STATE_LEFT_CORNER)
         {
-
             if (leftTurnSaved == 0)
             {
                 turnSequence[turnCount] = 'L';
                 turnCount++;
-
-                leftTurnSaved  = 1;
-            }         
-
-            Car_TurnLeft();
-
-            ultrasonicReadAllCm(distances, MAX_ECHO_US);
-            ultrasonicCheckThresholds(distances, thresholds);
-            leftSensor  = thresholds[0];
-            frontSensor = thresholds[1];
-            rightSensor = thresholds[2];
-
-            currentState = FSM_UpdateState(
-                                leftSensor,
-                                frontSensor,
-                                rightSensor,
-                                currentState, 
-                                NULL
-                           );                       
-        }
-
-
-        /* ========= POST LEFT CORNER ========= */
-
-        while (currentState == STATE_POST_LEFT_CORNER)
-        {
-            if (distances[2] < RIGHT_MIN_DISTANCE)
-            {
-                Car_RightAlign();
+                leftTurnSaved = 1;
             }
-            else if (distances[1] < POST_CORNER_MIN_DISTANCE)
-            {
+
+            if (frontSensor == 1)
+                Car_TurnLeft();
+            else if (distances[0] < LEFT_MIN_DISTANCE)
                 Car_LeftAlign();
-            }
+            else if (distances[2] < RIGHT_MIN_DISTANCE)
+                Car_RightAlign();
             else
-            {
                 Car_MoveForward();
-            }
 
             ultrasonicReadAllCm(distances, MAX_ECHO_US);
             ultrasonicCheckThresholds(distances, thresholds);
@@ -273,9 +217,9 @@ int main(void)
                                 leftSensor,
                                 frontSensor,
                                 rightSensor,
-                                currentState, 
+                                currentState,
                                 NULL
-                           );                    
+                           );
         }
 
 
